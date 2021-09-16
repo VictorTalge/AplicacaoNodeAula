203c10f9ce3b35303aed4306b535bc0ee1e6da53
Victor Talge <vt.talge@gmail.com>
2021-09-15 21:54:11 -0300

    Atualizado

---
+++ calculator.test.js
@@ -0,0 +1,15 @@
+/*import {
+    expect,
+    test
+} from '@jest/globals' */
+//import * as calculator from "./calculator.js";
+
+const calculator = require('./calculator');
+
+test("testando soma de 1+1", () => {
+    expect(calculator.soma(1, 1)).toBe(2);
+});
+
+test("testando divisao de 42/6", () => {
+    expect(calculator.divisao(42, 6)).toBe(7);
+});
